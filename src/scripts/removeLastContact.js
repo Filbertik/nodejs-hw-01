9703d9a57c944cfa5d9d41a01f709c289e87fcfe
filbertik <aton2019zvit@gmail.com>
2025-05-28 23:59:14 +0300

    fix 1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -3,17 +3,23 @@
 // removeLastContact();
 
 // step 8
-const { readContacts } = require('../utils/readContacts');
-const { writeContacts } = require('../utils/writeContacts');
+// const { readContacts } = require('../utils/readContacts');
+// const { writeContacts } = require('../utils/writeContacts');
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
 
-const removeLastContact = async () => {
+async function removeLastContact() {
   const contacts = await readContacts();
+
   if (contacts.length === 0) {
     console.log('No contacts to remove');
     return;
   }
+
   contacts.pop();
   await writeContacts(contacts);
-};
+
+  console.log('Last contact removed');
+}
 
 removeLastContact();
